c77a66d7cc2b9d404dc6da6922a0b5082b31e394
jaehyuk <fruit9370@naver.com>
2023-04-19 13:10:12 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/likelion/javaproject2/CrawlDate.java
@@ -0,0 +1,11 @@
+package com.likelion.javaproject2;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
