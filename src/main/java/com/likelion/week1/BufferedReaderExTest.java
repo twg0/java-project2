eecc3b5310f427155b87594668121f4a990f5506
jaehyuk <fruit9370@naver.com>
2023-04-19 15:29:12 +0900

    feat: Scanner 추가 - ScannerEx, ScannerExTest

--- src/main/java/com/likelion/week1/BufferedReaderExTest.java
+++ src/main/java/com/likelion/week1/BufferedReaderExTest.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class BufferedReaderExTest {
     public static void main(String[] args) throws IOException {
         BufferedReaderEx br = new BufferedReaderEx();
-        br.readALine();
+        br.plusTwoNumbers();
     }
-
 }
