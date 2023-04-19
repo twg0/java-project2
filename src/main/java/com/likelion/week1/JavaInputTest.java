08f16e6f7edba9c485c2b4bf0ad30a49224639a6
jaehyuk <fruit9370@naver.com>
2023-04-19 14:19:39 +0900

    feat: read 메소드 추가 - JavaInput, JavaInputTest

---
+++ src/main/java/com/likelion/week1/JavaInputTest.java
@@ -0,0 +1,10 @@
+package com.likelion.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput javaInput = new JavaInput();
+        javaInput.readTwoChars();
+    }
+}
