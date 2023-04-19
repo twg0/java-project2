25349e73cec99fa810348445a4df742a0040a45a
jaehyuk <fruit9370@naver.com>
2023-04-19 14:45:28 +0900

    feat: BufferedReader 추가 - BufferedReaderEx, BufferedReaderExTest

---
+++ src/main/java/com/likelion/week1/BufferedReaderExTest.java
@@ -0,0 +1,11 @@
+package com.likelion.week1;
+
+import java.io.IOException;
+
+public class BufferedReaderExTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReaderEx br = new BufferedReaderEx();
+        br.readALine();
+    }
+
+}
