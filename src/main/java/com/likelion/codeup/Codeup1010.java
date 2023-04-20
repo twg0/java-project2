8484146c414b89aa84cbf16a16607e80f508c2a1
jaehyuk <fruit9370@naver.com>
2023-04-20 09:39:29 +0900

    feat: ScannerStr 추가 - ScannerStrEx, ScannerStrExTest

---
+++ src/main/java/com/likelion/codeup/Codeup1010.java
@@ -0,0 +1,10 @@
+package com.likelion.codeup;
+
+import java.util.*;
+
+public class Codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
+}
---
+++ src/main/java/com/likelion/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package com.likelion.week1.day4;
+
+import com.likelion.week1.ScannerEx;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx se = new ScannerStrEx();
+        se.inputStringsAndPlus();
+    }
+}
