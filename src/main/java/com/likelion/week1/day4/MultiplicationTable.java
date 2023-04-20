700d18f85442caa76131715655307db26eb0f6bf
jaehyuk <fruit9370@naver.com>
2023-04-20 11:29:52 +0900

    feat: MultiplicationTable 추가 - MultiplicationTable

--- src/main/java/com/likelion/week1/day4/MultiplicationTable.java
+++ src/main/java/com/likelion/week1/day4/MultiplicationTable.java
@@ -3,6 +3,6 @@ package com.likelion.week1.day4;
 public class MultiplicationTable {
     public static void main(String[] args) {
         System.out.println("2" + "*" + "1" + "=" + "2");
-        System.out.printf("%d * %d = %d", 2, 2, 4);
+        System.out.printf("%d * %d = %d", 2, 2, 2*2);
     }
 }
