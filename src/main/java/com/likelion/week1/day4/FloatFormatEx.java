3f793bfc8b9d57e8600ededc176890283fd7e468
jaehyuk <fruit9370@naver.com>
2023-04-20 13:37:14 +0900

    feat: AverageEx 추가 - AverageEx

---
+++ src/main/java/com/likelion/week1/day4/FloatFormatEx.java
@@ -0,0 +1,9 @@
+package com.likelion.week1.day4;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.1f\n", 1.44);
+        System.out.printf("%.2f\n", 3.78987);
+    }
+}
