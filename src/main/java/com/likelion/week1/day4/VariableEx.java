23f39b62eaaa555c0e62dabcf9bab7244287cee7
jaehyuk <fruit9370@naver.com>
2023-04-20 15:12:25 +0900

    feat: VariableEx 추가 - VariableEx

---
+++ src/main/java/com/likelion/week1/day4/VariableEx.java
@@ -0,0 +1,16 @@
+package com.likelion.week1.day4;
+
+import com.likelion.javaproject2.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+
+        PrintHello printHello;
+        printHello = new PrintHello();
+
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
