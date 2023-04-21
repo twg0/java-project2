e11d9704b788e7b7d873435381f541f82027af55
jaehyuk <fruit9370@naver.com>
2023-04-21 14:14:15 +0900

    feat: 분기를 확인하기 위해 주석 추가 - Codeup1001 수정

--- src/main/java/com/likelion/codeup/Codeup1001.java
+++ src/main/java/com/likelion/codeup/Codeup1001.java
@@ -1,6 +1,7 @@
 package com.likelion.codeup;
 
 public class Codeup1001 {
+//    분기된 브랜치를 소스트리에서 눈으로 확인하기 위해 주석 추가
     public static void main(String[] args) {
         System.out.println("Hello");
     }
