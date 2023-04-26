6b367458550faf71c962c59673c1bf9fb0b6c228
jaehyuk <fruit9370@naver.com>
2023-04-26 15:38:38 +0900

    feat: BankAccount, LikeAccumulate 추가 - BankAccount, LikeAccumulate

--- src/main/java/com/likelion/week2/day3/BankAccount.java
+++ src/main/java/com/likelion/week2/day3/BankAccount.java
@@ -3,10 +3,10 @@ package com.likelion.week2.day3;
 public class BankAccount {
     public static void main(String[] args) {
         int myAccount = 1_000_000;
-        myAccount = myAccount + 500_000;
+        myAccount = myAccount - 500_000;
         System.out.println(myAccount);
 
-        myAccount = myAccount + 400_000;
+        myAccount = myAccount + 4_000_000;
         System.out.println(myAccount);
     }
 }
