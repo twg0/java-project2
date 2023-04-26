35f9c37f869c8f108ebfb669de3fc5551e18dc0b
jaehyuk <fruit9370@naver.com>
2023-04-26 11:17:42 +0900

    feat: TwoDimArray 추가 - TwoDimArray

---
+++ src/main/java/com/likelion/week2/day3/DefineArr2.java
@@ -0,0 +1,19 @@
+package com.likelion.week2.day3;
+
+import java.util.Arrays;
+
+public class DefineArr2 {
+    public static void main(String[] args) {
+        int[] iArr = new int[]{1, 2, 3, 8};
+        System.out.println("iArr[0] = " + iArr[0]);
+
+        int[][] arr2 = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+        System.out.println(Arrays.toString(arr2[0]));
+        System.out.println(Arrays.toString(arr2[1]));
+        System.out.println(Arrays.toString(arr2[2]));
+    }
+}
