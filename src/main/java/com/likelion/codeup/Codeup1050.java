be81279c684d70475c31978f86b2e8414d471244
jaehyuk <fruit9370@naver.com>
2023-04-26 11:33:31 +0900

    feat: printArray 추가 - TwoDimArray

--- src/main/java/com/likelion/codeup/Codeup1050.java
+++ src/main/java/com/likelion/codeup/Codeup1050.java
@@ -3,11 +3,11 @@ package com.likelion.codeup;
 
 import java.util.Scanner;
 
-public class Codeup1047 {
+public class Codeup1050 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int sum = sc.nextInt();
-        int eNum = sc.nextInt();
-        System.out.println(sum << eNum);
+        int numA = sc.nextInt();
+        int numB = sc.nextInt();
+        System.out.println(numA >= numB ? 1 : 0);
     }
 }
--- src/main/java/com/likelion/week2/day3/TwoDimArray.java
+++ src/main/java/com/likelion/week2/day3/TwoDimArray.java
@@ -3,14 +3,29 @@ package com.likelion.week2.day3;
 import java.util.Arrays;
 
 public class TwoDimArray {
-    public static void main(String[] args) {
 
-        int[][] arr = new int[3][3];
-        int[] arr0 = arr[0];
-        arr0[0] = 1;
+    public static void printArray(int[][] arr) {
 
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
+        System.out.println("---------");
+    }
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][3];
+
+        printArray(arr);
+
+        arr[0][0] = 1;
+        arr[0][2] = 2;
+        arr[1][0] = 3;
+
+        printArray(arr);
+
+        arr[1][1] = 5;
+
+        printArray(arr);
     }
 }
