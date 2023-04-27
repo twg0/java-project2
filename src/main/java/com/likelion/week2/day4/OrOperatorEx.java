58ae569d4383829ea128afa2d1506f00db3d6793
jaehyuk <fruit9370@naver.com>
2023-04-27 13:09:47 +0900

    feat: Or, Not 실습 추가 - OrOperatorEx - OrOperatorEx2 - CheckWhetherAdult

--- src/main/java/com/likelion/week2/day4/OrOperatorEx.java
+++ src/main/java/com/likelion/week2/day4/OrOperatorEx.java
@@ -1,18 +1,15 @@
 package com.likelion.week2.day4;
 
-public class AndOperator {
+public class OrOperatorEx {
     public static void main(String[] args) {
-
-        boolean result = true && true;
+        boolean result = true || true;
+        System.out.printf("%b\n", result);
+        result = true || false;
+        System.out.printf("%b\n", result);
+        result = false || true;
+        System.out.printf("%b\n", result);
+        result = false || false;
         System.out.printf("%b\n", result);
 
-        result = true && false;
-        System.out.printf("%b\n", result);
-
-        result = false && true;
-        System.out.printf("%b\n", result);
-
-        result = false && false;
-        System.out.printf("%b\n", result);
     }
 }
