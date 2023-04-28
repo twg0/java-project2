aa55cd43d0668077336a08a9a96319107c15a0f7
jaehyuk <fruit9370@naver.com>
2023-04-28 10:37:37 +0900

    feat: BCompanyHRSystem,IfWithLogicalOperator  추가

--- src/main/java/com/likelion/codeup/Codeup1077.java
+++ src/main/java/com/likelion/codeup/Codeup1077.java
@@ -1,18 +1,14 @@
 package com.likelion.codeup;
 
 
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Codeup1077 {
-    public static void main(String[] args) throws IOException {
-        InputStreamReader is = new InputStreamReader(System.in);
-        int num = is.read();
-        int start = 'a';
-        while(true) {
-            System.out.printf("%c ",start);
-            if(start == num) break;
-            start++;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i);
         }
     }
 }
