70105deafde19cb92ddef4c8ab2491d6fb1400a3
jaehyuk <fruit9370@naver.com>
2023-05-01 13:14:24 +0900

    feat: 10 미만의 짝수 홀수 추가 - ForLoopForwardStep

---
+++ src/main/java/com/likelion/week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,12 @@
+package com.likelion.week3.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i+=2) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
