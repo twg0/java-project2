0b5c87529357ccfe329225862dcbce691d2f90b1
twg0 <fruitsjh@gmail.com>
2023-05-01 15:13:48 +0900

    feat: Codeup1080 추가 - Codeup1080

--- src/main/java/com/likelion/codeup/Codeup1080.java
+++ src/main/java/com/likelion/codeup/Codeup1080.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Codeup1080 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         int num = sc.nextInt();
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
