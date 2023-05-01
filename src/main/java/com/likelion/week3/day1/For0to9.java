0d8709fc670681dde231b6c49d248e5d4ffd04cd
jaehyuk <fruit9370@naver.com>
2023-05-01 10:09:15 +0900

    feat: For0to9 추가

---
+++ src/main/java/com/likelion/week3/day1/For0to9.java
@@ -0,0 +1,9 @@
+package com.likelion.week3.day1;
+
+public class For0to9 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
