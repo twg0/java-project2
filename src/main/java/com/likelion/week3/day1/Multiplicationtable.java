0cac64a30d070762cdff9a35b5d9b6412768b1c4
twg0 <fruitsjh@gmail.com>
2023-05-01 14:20:53 +0900

    feat: 구구단 2 ~ 4단 추가 - Multiplicationtable

--- src/main/java/com/likelion/week3/day1/Multiplicationtable.java
+++ src/main/java/com/likelion/week3/day1/Multiplicationtable.java
@@ -2,8 +2,10 @@ package com.likelion.week3.day1;
 
 public class Multiplicationtable {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++) {
-            System.out.printf("%d * %d = %d\n", 2, i, 2 * i);
+        for (int j = 2; j <= 4; j++) {
+            for (int i = 1; i <= 9; i++) {
+                System.out.printf("%d * %d = %d\n", j, i, j * i);
+            }
         }
     }
 }
