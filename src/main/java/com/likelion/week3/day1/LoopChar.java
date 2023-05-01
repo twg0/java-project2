79d24973fc87e9f62c2d0981f21fcb3c55660feb
twg0 <fruitsjh@gmail.com>
2023-05-01 13:43:11 +0900

    feat: LoopChar 추가 - LoopChar

--- src/main/java/com/likelion/week3/day1/LoopChar.java
+++ src/main/java/com/likelion/week3/day1/LoopChar.java
@@ -11,6 +11,12 @@ public class LoopChar {
             System.out.printf("%c\n", c);
         }
 
+        for (char c = '가'; c <= '힣';c++) {
+            System.out.printf("%c ", c);
+        }
+        System.out.println();
+
+
         System.out.printf("%c\n", 222222222);
     }
 }
