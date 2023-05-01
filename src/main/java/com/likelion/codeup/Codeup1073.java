eb4f5b7d47d2ff743264df2d26665490344631a0
jaehyuk <fruit9370@naver.com>
2023-05-01 11:38:24 +0900

    feat: Codeup1073, Codeup1074, Codeup1075 추가 - Codeup1073 - Codeup1074 - Codeup1075

--- src/main/java/com/likelion/codeup/Codeup1073.java
+++ src/main/java/com/likelion/codeup/Codeup1073.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Codeup1073 {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
+
         while(true) {
             int num = sc.nextInt();
             if(num == 0) break;
--- src/main/java/com/likelion/codeup/Codeup1074.java
+++ src/main/java/com/likelion/codeup/Codeup1074.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Codeup1074 {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
+
         int num = sc.nextInt();
         while(true) {
             System.out.println(num--);
