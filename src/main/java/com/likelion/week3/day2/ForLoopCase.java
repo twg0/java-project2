b226a5a39f0a0917caf2b8ce373ceb019a329441
twg0 <fruitsjh@gmail.com>
2023-05-02 10:48:24 +0900

    feat: ForLoopCase 추가 - ForLoopCase

---
+++ src/main/java/com/likelion/week3/day2/ForLoopCase.java
@@ -0,0 +1,18 @@
+package com.likelion.week3.day2;
+
+public class ForLoopCase {
+    public static void main(String[] args) {
+
+//        for (int i = 0; 1 == 1; i++) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 0; i * 2 == 10; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i * i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+}
