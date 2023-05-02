e5906d302d5873e859c8081be19a9b780dab710a
twg0 <fruitsjh@gmail.com>
2023-05-02 15:31:58 +0900

    feat: 자릿수 구하기 추가 - IndexNum

---
+++ src/main/java/com/likelion/week3/day2/IndexNum.java
@@ -0,0 +1,17 @@
+package com.likelion.week3.day2;
+
+import java.util.Scanner;
+
+public class IndexNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int cnt = 0;
+        while(num > 0) {
+            num /= 10;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
