8d99eca7e6f84eca46f378db9dcb7b0f147b0f9b
twg0 <fruitsjh@gmail.com>
2023-05-02 14:40:29 +0900

    feat: 자릿수 합 추가 - SumIndexNum

---
+++ src/main/java/com/likelion/programmers/SumIndexNum.java
@@ -0,0 +1,12 @@
+package com.likelion.programmers;
+
+public class SumIndexNum {
+    public int solution(int n) {
+        int answer = 0;
+        while(n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
