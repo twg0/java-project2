97308aa0eee76418ad2ffa804c147121a02baba4
twg0 <fruitsjh@gmail.com>
2023-05-02 11:02:52 +0900

    feat: Codeup1083 추가 - Codeup1083

--- src/main/java/com/likelion/codeup/Codeup1083.java
+++ src/main/java/com/likelion/codeup/Codeup1083.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Codeup1083 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int num = sc.nextInt();
 
         for(int i = 1;i <= num;i++) {
