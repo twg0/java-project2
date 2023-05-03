17615d46de04ccc30cbaa0622b16ce4fd1452d16
twg0 <fruitsjh@gmail.com>
2023-05-03 10:07:56 +0900

    feat: 코드업 1071 1073 추가 - Codeup1071 - Codeup1073

--- src/main/java/com/likelion/codeup/Codeup1071.java
+++ src/main/java/com/likelion/codeup/Codeup1071.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class Codeup1071 {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int numN = sc.nextInt();
-        for (int i = 0; i < numN; i++) {
-            System.out.println(sc.nextInt());
-        }
+        int num;
+        do {
+            num = sc.nextInt();
+            if(num == 0) break;
+            System.out.println(num);
+        } while(num != 0);
     }
 }
--- src/main/java/com/likelion/codeup/Codeup1073.java
+++ src/main/java/com/likelion/codeup/Codeup1073.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 public class Codeup1073 {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-
-        while(true) {
-            int num = sc.nextInt();
+        int num;
+        do {
+            num = sc.nextInt();
             if(num == 0) break;
             System.out.println(num);
-        }
+        } while(num != 0);
     }
 }
