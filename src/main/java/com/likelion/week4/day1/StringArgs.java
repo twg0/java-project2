1bcc0a8f34b4cd9563b3d151c695d417c7356154
twg0 <fruitsjh@gmail.com>
2023-05-08 15:46:31 +0900

    feat: StringArgs 추가 - StringArgs

---
+++ src/main/java/com/likelion/week4/day1/StringArgs.java
@@ -0,0 +1,9 @@
+package com.likelion.week4.day1;
+
+import java.util.Arrays;
+
+public class StringArgs {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
