d6ab58d636522ad6355f018cb96332aaa6ad8a1b
twg0 <fruitsjh@gmail.com>
2023-05-08 11:08:22 +0900

    feat: SpaceInvaders 추가 - SpaceInvaders - SpaceInvadersTest

---
+++ src/main/java/com/likelion/week4/day1/SpaceInvaders.java
@@ -0,0 +1,12 @@
+package com.likelion.week4.day1;
+
+public class SpaceInvaders {
+    int location;
+    public void moveLeft() {
+        location -= 1;
+    }
+
+    public void moveRight() {
+        location += 1;
+    }
+}
---
+++ src/main/java/com/likelion/week4/day1/SpaceInvadersTest.java
@@ -0,0 +1,15 @@
+package com.likelion.week4.day1;
+
+public class SpaceInvadersTest {
+    public static void main(String[] args) {
+        SpaceInvaders si = new SpaceInvaders();
+        si.moveLeft();
+        System.out.println("si.location = " + si.location);
+        si.moveLeft();
+        System.out.println("si.location = " + si.location);
+        si.moveLeft();
+        System.out.println("si.location = " + si.location);
+        si.moveRight();
+        System.out.println("si.location = " + si.location);
+    }
+}
