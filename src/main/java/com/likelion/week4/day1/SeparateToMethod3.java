13545cb0d9df3e5e38a772be8e931007dae5ffeb
twg0 <fruitsjh@gmail.com>
2023-05-09 11:42:39 +0900

    feat: printArr() 리팩토링 실습 추가 - SeparateToMethod - SeparateToMethod3

---
+++ src/main/java/com/likelion/week4/day1/SeparateToMethod3.java
@@ -0,0 +1,22 @@
+package com.likelion.week4.day1;
+
+import java.util.Arrays;
+
+public class SeparateToMethod3 {
+    public static void printArray(int[][] arr) {
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+
+        printArray(arr);
+    }
+
+}
